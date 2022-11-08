9a9a36bd846ca5fa5d37d3eee3145ff1b947dc37
TigMart <tig.mar@yandex.ru>
2022-11-08 22:29:54 +0400

    Responsive

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -1,49 +1,48 @@
-import type { NextPage } from 'next'
-import { GlobalProps } from '../../../pages'
-import { ActionTypeEnum } from '../../types/enums'
-import { FileUpload } from './fileUpload'
-import { MakeToEdit } from './makeToEdit'
-import styles from '../styles/section.module.css'
+import type { NextPage } from "next";
+import { GlobalProps } from "../../../pages";
+import { ActionTypeEnum } from "../../types/enums";
+import { FileUpload } from "./fileUpload";
+import { MakeToEdit } from "./makeToEdit";
+import styles from "../styles/processPart.module.css";
 
+export const Section: any = (props: GlobalProps) => {
+  return (
+    <>
 
-export const Section: any = (props: GlobalProps ) => {
-    return (
-        <div className={styles.sectionPart}>
-                {props.isReadyToEdit ? 
-                    <MakeToEdit 
-                        {...props}
-                    />
-                :
-                    <FileUpload 
-                        setToEdit = {props.setToEdit} 
-                        setImagePath={props.setImagePath} 
-                        setToFetching = {props.setToFetching}/>
-                }
-            {/* {error ? 
+      <div className={styles.containerProcess}>
+        {props.isReadyToEdit ? (
+          <MakeToEdit {...props} />
+        ) : (
+          <FileUpload
+            setToEdit={props.setToEdit}
+            setImagePath={props.setImagePath}
+            setToFetching={props.setToFetching}
+          />
+        )}
+        {/* {error ?
             <ErrorComponent error ={error} deleteImg={deleteImg} aboutImage={aboutImage}
-            />  
-          : isReadyForShare ? 
-            <MakeForShare 
-              aboutImage={aboutImage} 
+            />
+          : isReadyForShare ?
+            <MakeForShare
+              aboutImage={aboutImage}
               isFetching={isFetching}
               deleteImg={deleteImg}
               getResult={getResult}
             /> :
-          isReadyForEdit ? 
-            <MakeToEdit 
-              aboutImage={aboutImage} 
+          isReadyForEdit ?
+            <MakeToEdit
+              aboutImage={aboutImage}
               isFetching={isFetching}
               cropImage={cropImage}
               setToFetching={setToFetching}
-            /> : 
-            <FileUpload 
-              setImagePath={setImagePath} 
-              setForEdit={setForEdit} 
+            /> :
+            <FileUpload
+              setImagePath={setImagePath}
+              setForEdit={setForEdit}
               setToFetching={setToFetching}
             />
           } */}
-        </div>
-    )
-}
-
-
+      </div>
+    </>
+  );
+};
